07d764f07b358b47616f58ff0e7ceeed0915e94c
Tom <tom-vautray@hotmail.fr>
2021-08-15 14:12:17 +0200

    Changed class MainActivity's location

---
+++ app/src/main/java/com/example/worldofdrinkraft/architecture/presentation/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.worldofdrinkraft.architecture.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.worldofdrinkraft.R;
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
