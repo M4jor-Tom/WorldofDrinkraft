63e3a7cde7086dc25998e14561bca4f3eade9a01
Tom <tom-vautray@hotmail.fr>
2021-08-15 14:13:54 +0200

    Added players setup activity

---
+++ app/src/main/java/com/example/worldofdrinkraft/architecture/presentation/PlayersSetupActivity.java
@@ -0,0 +1,17 @@
+package com.example.worldofdrinkraft.architecture.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.worldofdrinkraft.R;
+
+public class PlayersSetupActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_players_setup);
+    }
+}
