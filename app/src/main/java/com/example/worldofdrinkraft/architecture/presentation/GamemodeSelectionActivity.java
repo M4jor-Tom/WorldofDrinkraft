29645040f02abd481693dd1d61f7193743277e08
Tom <tom-vautray@hotmail.fr>
2021-08-26 20:06:04 +0200

    [NF] Added OnItemClickEventListener to Gamemode select activity spinner

--- app/src/main/java/com/example/worldofdrinkraft/architecture/presentation/GamemodeSelectionActivity.java
+++ app/src/main/java/com/example/worldofdrinkraft/architecture/presentation/GamemodeSelectionActivity.java
@@ -1,12 +1,15 @@
 package com.example.worldofdrinkraft.architecture.presentation;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.worldofdrinkraft.R;
+import com.example.worldofdrinkraft.architecture.business.IBusinessContainer;
 import com.example.worldofdrinkraft.game.Game;
 import com.example.worldofdrinkraft.gamemode.AbstractGamemode;
 import com.example.worldofdrinkraft.gamemode.GamemodeFactory;
@@ -27,6 +30,19 @@ public class GamemodeSelectionActivity extends AppCompatActivity
                         R.layout.adapter_gamemode,
                         GamemodeFactory.getAllGamemodes()
                 )
+        );
+
+        gamemodeSelectionSpinner.setOnItemClickListener(
+                new AdapterView.OnItemClickListener()
+                {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+                    {
+                        IBusinessContainer.getInstance().setGamemodeType(
+                                GamemodeFactory.getAllGamemodes().get(position).getGamemodeType()
+                        );
+                    }
+                }
         );
     }
 }
